9db895671e2bcdc8b5f1c3de04a7260444ef5575
leonardofassini <leehtironi@gmail.com>
2020-06-14 12:03:37 -0300

    removed god knows what those decorators were

--- .babelrc.js
+++ .babelrc.js
@@ -1,12 +1,12 @@
 const presets = ['@babel/react', '@babel/typescript', ['@babel/env', { modules: false }]];
 
 const plugins = [
-	['@babel/plugin-proposal-decorators', { legacy: true }],
-	'@babel/plugin-proposal-class-properties',
-	'babel-plugin-styled-components',
-	'@babel/plugin-proposal-object-rest-spread',
-	'@babel/plugin-proposal-optional-chaining',
-	'@babel/plugin-proposal-nullish-coalescing-operator',
+  ['@babel/plugin-proposal-decorators', { legacy: true }],
+  '@babel/plugin-proposal-class-properties',
+  'babel-plugin-styled-components',
+  '@babel/plugin-proposal-object-rest-spread',
+  '@babel/plugin-proposal-optional-chaining',
+  '@babel/plugin-proposal-nullish-coalescing-operator',
 ];
 
 module.exports = { presets, plugins };
